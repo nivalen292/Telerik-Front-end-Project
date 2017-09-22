3d7ebc268a027ee38c2402005828f6c39d6f3d14
nivalen292 <nivalen292@outlook.com>
2017-09-22 18:33:01 +0300

    gulp fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,26 @@ const babel = require('gulp-babel');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const pump = require('pump');
+const concat = require('gulp-concat');
+const clean = require('gulp-clean');
 
 gulp.task('compile:js', () => {
-    return gulp.src(['./scripts/*.js', '!./scripts/bootstrap.min.js'])
+    return gulp.src(['./scripts/*.js', '!./scripts/bootstrap.min.js', '!./scripts/jquery.min.js'])
         .pipe(babel({presets: ['env']}))
         .pipe(gulp.dest('./tmp/scripts'));
 });
 
 gulp.task('minify:css', () => {
     return gulp.src(['./styles/*.css', '!./styles/bootstrap.min.css'])
-        .pipe(cleanCSS(cleanCSS({compatibility: 'ie8'})))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('./dist/styles'));
 });
 
-gulp.task('minify:js', ['compile:js'] ,() => {
+gulp.task('minify:js', ['compile:js'], () => {
     return pump([
         gulp.src('./tmp/scripts/*.js'),
         uglify(),
+        concat('all.js'), // trial
         gulp.dest('./dist/scripts')
     ]);
-});
-
-
-gulp.task('build:js', ['minify:js'], () => {
-    return gulp.src(['./dist/scripts/neshto.js', './dist/scripts/neshto2.js'])
-        .pipe(cleanCSS(cleanCSS({compatibility: 'ie8'})))
-        .pipe(gulp.dest('./dist/styles'));
 });
